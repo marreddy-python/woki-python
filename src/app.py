fd0ab5ad9df7ca0b63ef562fea7e7df0f3bd84e8
marreddy-python <marreddy@becknf.com>
2020-01-02 12:20:37 +0530

    rm cors

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 # from flask_cors import CORS
-from flask.ext.cors import CORS
 from .config import app_config
 from .models import db
 from .views.BlogpostView import user_api as user_blueprint
@@ -10,10 +9,9 @@ from .views.Users_details import user_details_api as user_details_blueprint
 
 def create_app(env_name):
   
-  
   # app initiliazation
   app = Flask(__name__)
-  CORS(app)
+  # CORS(app)
 
   app.config.from_object(app_config[env_name])
   
